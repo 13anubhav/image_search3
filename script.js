835a1da79aad04c29a12f6aca991001b6976d7c3
Anubhav Chaudhary <126059931+13anubhav@users.noreply.github.com>
2023-05-17 17:41:12 +0530

    Create script.js --> 17-05-23 --> 05.41 PM

---
+++ script.js
@@ -0,0 +1,59 @@
+
+     // Replace with your RapidAPI Bing Image Search API key
+     const apiKey = 'c3e7e0f9a2msh9541025fef72972p19b90ejsn2b4a44807bbe';
+
+     const searchForm = document.querySelector('#search-form');
+     const imageContainers = [
+       document.querySelector('#image-container-1'),
+       document.querySelector('#image-container-2'),
+       document.querySelector('#image-container-3')
+     ];
+     
+     searchForm.addEventListener('submit', async (e) => {
+       e.preventDefault();
+     
+       // Clear previous search results
+       imageContainers.forEach(container => container.innerHTML = '');
+     
+     //   const searchTerms = [
+     //     document.querySelector('#search-term-1').value,
+     //     document.querySelector('#search-term-2').value,
+     //     document.querySelector('#search-term-3').value
+     //   ];
+     
+       const searchTerms =['leopard', 'Paris', 'SupeMan'];
+     // let search-term-1 ='car';
+     // let search-term-2 = 'bike';
+     
+     // let search-term-3 ='pikachu';
+     
+     
+       // Perform image search for each search term
+       for (let i = 0; i < searchTerms.length; i++) {
+         const searchTerm = searchTerms[i];
+         const container = imageContainers[i];
+     
+         // Perform search using Bing Image Search API
+         const response = await fetch(
+           `https://bing-image-search1.p.rapidapi.com/images/search?q=${encodeURIComponent(searchTerm)}&count=1`,
+     {
+     headers: {
+     'x-rapidapi-key': apiKey,
+     'x-rapidapi-host': 'bing-image-search1.p.rapidapi.com',
+     },
+     }
+     );
+     
+     const data = await response.json();
+     
+     // Render search results
+     data.value.forEach(result => {
+       const img = document.createElement('img');
+       img.src = result.thumbnailUrl;
+       img.alt = result.name;
+       container.appendChild(img);
+     });
+     
+     }
+     });
+     
